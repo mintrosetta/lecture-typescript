83f502d12816de4e148e8111559e33dfeec09c5f
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-01-04 20:18:22 +0700

    lecture: enum

---
+++ source/004_enum.ts
@@ -0,0 +1,17 @@
+enum Month {
+    Jan = 1,
+    Feb,
+    Mar,
+    Apr,
+    May,
+    Jun,
+    Jul,
+    Aug,
+    Sep,
+    Oct,
+    Nov,
+    Dev
+};
+
+const month: Month = Month.Jul;
+console.log(month);
